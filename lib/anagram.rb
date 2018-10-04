cb1b9dc020087eb10732ad12669379d4a9fb80e2
larryvsdata <ermanbekaroglu@gmail.com>
2018-10-04 00:55:32 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word=word
+  end
+  
+    def isAnagram(word1,word2)
+    
+    word1.split("").sort == word2.split("").sort
+  end
+  
+  def match(phrase)
+    
+    phrase.map{ |x| x if self.isAnagram(x,@word) }.compact
+  end
+  
+
+  
+  
+end
